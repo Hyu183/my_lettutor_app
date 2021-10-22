82668fc3d12c24dd12299961b0848866bd808a0d
l4zyc0d3r <huytrquang1103@gmail.com>
2021-10-22 10:19:35 +0700

    basic tutors page

--- lib/home/teacher_list/tutors.dart
+++ lib/home/teacher_list/tutors.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:my_lettutor_app/widgets/speciality_badge_list.dart';
 
 class Tutors extends StatelessWidget {
-  const Tutors({Key? key}) : super(key: key);
+  static const routeName = '/tutors';
+  Tutors({Key? key}) : super(key: key);
+
+  List<String> specialities = [
+    'All',
+    'English for kids',
+    'English for Business',
+    'Conversational',
+    'STARTERS',
+    'MOVERS',
+    'FLYERS',
+    'KET',
+    'PET',
+    'IELTS',
+    'TOEFL',
+    'TOEIC',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +32,45 @@ class Tutors extends StatelessWidget {
           ),
         ),
       ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(color: Colors.grey),
+              child: Row(
+                children: const [
+                  Icon(Icons.search),
+                  Flexible(
+                      child: TextField(
+                          decoration: InputDecoration(hintText: 'search',),))
+                ],
+              ),
+            ),
+            SpecialityBadgeList(specialityList: specialities),
+          ],
+        ),
+      ),
+      //   body: SizedBox(
+      //     width: 500,
+      //     child: Column(
+      //       children: [
+      //         SizedBox(
+      //           width: 300,
+      //           child: Row(
+      //             children: [
+      //               const Icon(Icons.search),
+      //               TextField(
+      //                 decoration:
+      //                     InputDecoration(hintText: 'Enter a search term'),
+      //               ),
+      //             ],
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
     );
   }
 }
