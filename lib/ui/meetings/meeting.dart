e6954fd6b29ddb0f77fb9af4329e251dbf0d9ff4
l4zyc0d3r <huytrquang1103@gmail.com>
2022-01-20 16:58:44 +0700

    count time class started

--- lib/ui/meetings/meeting.dart
+++ lib/ui/meetings/meeting.dart
@@ -1,43 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:my_lettutor_app/models/user_schedule.dart';
 
-import 'package:my_lettutor_app/ui/meetings/timer_clock.dart';
+import 'package:my_lettutor_app/ui/meetings/timer_clock_decrease.dart';
+import 'package:my_lettutor_app/ui/meetings/timer_clock_increase.dart';
 
 class Meeting extends StatelessWidget {
-    static const  routeName = '/meeting';
+  static const routeName = '/meeting';
   final UserSchedule userSchedule;
   const Meeting({Key? key, required this.userSchedule}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var remainTime = DateTime.fromMillisecondsSinceEpoch(
+            userSchedule.scheduleDetailInfo!.startPeriodTimestamp!)
+        .toLocal()
+        .difference(DateTime.now())
+        .inSeconds;
+    remainTime = remainTime > 0 ? remainTime : 0;
+
     return Scaffold(
       body: Stack(
-        alignment: Alignment.center,
+        alignment: remainTime == 0 ? Alignment.topLeft : Alignment.center,
         children: [
           const SizedBox(height: double.infinity, width: double.infinity),
-          Container(
-              width: 260,
-              height: 80,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.black38,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Text(
-                      'Lesson will be started after',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
+          remainTime == 0
+              ? Container(
+                  width: 50,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.black38,
+                  ),
+                  margin: const EdgeInsets.only(top: 40, left: 20),
+                  child: Center(
+                    child: TimerClockIncrease(
+                      startTime: userSchedule
+                          .scheduleDetailInfo!.startPeriodTimestamp!,
                     ),
-                    TimerClock(remainderInSeconds: 3605),
-                  ],
-                ),
-              )),
+                  ),
+                )
+              : Container(
+                  width: 260,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.black38,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        const Text(
+                          'Lesson will be started after',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                        TimerClockDescrease(remainderInSeconds: remainTime),
+                      ],
+                    ),
+                  )),
         ],
       ),
     );
--- lib/ui/meetings/timer_clock_increase.dart
+++ lib/ui/meetings/timer_clock_increase.dart
@@ -2,27 +2,30 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-class TimerClock extends StatefulWidget {
-  final int remainderInSeconds;
-  const TimerClock({
+class TimerClockIncrease extends StatefulWidget {
+  final int startTime;
+  const TimerClockIncrease({
     Key? key,
-    required this.remainderInSeconds,
+    required this.startTime,
   }) : super(key: key);
 
   @override
-  _TimerClockState createState() => _TimerClockState();
+  _TimerClockIncreaseState createState() => _TimerClockIncreaseState();
 }
 
-class _TimerClockState extends State<TimerClock> {
+class _TimerClockIncreaseState extends State<TimerClockIncrease> {
   late Duration duration;
   final unit = 1;
   Timer? timer;
+// late DateTime timeLearned;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    duration = Duration(seconds: widget.remainderInSeconds);
+    duration = DateTime.now().difference(
+        DateTime.fromMillisecondsSinceEpoch(widget.startTime).toLocal());
+    // duration = Duration(seconds: widget.);
     startTimer();
   }
 
@@ -33,35 +36,32 @@ class _TimerClockState extends State<TimerClock> {
     super.dispose();
   }
 
-  void decreaseTimer() {
+  void increaseTimer() {
     setState(() {
-      final newVal = duration.inSeconds - unit;
-      if (newVal < 0) {
-        timer!.cancel();
-      } else {
-        duration = Duration(seconds: newVal);
-      }
+      final newVal = duration.inSeconds + unit;
+
+      duration = Duration(seconds: newVal);
     });
   }
 
   void startTimer() {
     timer = Timer.periodic(
       const Duration(seconds: 1),
-      (_) => decreaseTimer(),
+      (_) => increaseTimer(),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final hours = twoDigits(duration.inHours);
+    // final hours = twoDigits(duration.inHours);
     final minutes = twoDigits(duration.inMinutes.remainder(60));
     final seconds = twoDigits(duration.inSeconds.remainder(60));
     return Text(
-      '$hours:$minutes:$seconds',
+      '$minutes:$seconds',
       style: const TextStyle(
         color: Colors.white,
-        fontSize: 25,
+        fontSize: 15,
         fontWeight: FontWeight.bold,
       ),
     );
